95dd3bdc0c3cc1e2f06ddc85e2813ab4b92d0c94
Abdelrahman Ashraf <8effort@gmail.com>
2026-06-08 18:35:49 +0300

    firebase android setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'dart:io';
+
 import 'package:asroo_store/asro_store_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: 'AIzaSyBJ2nzlcWxEUFsI1WV0gnHrBIAF2C-hqts',
+            appId: '1:109844083865:android:d30c35ecac315c1e4f3408',
+            messagingSenderId: '109844083865',
+            projectId: 'asroo-store-973cf',
+          ),
+        )
+      : await Firebase.initializeApp();
+
   runApp(const AsrooStoreApp());
 }
-
-
